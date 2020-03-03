3403eaafab9c72eac969833ee5c0ce72c8abf708
munificent-cast-9719 <agomezdemen15@gmail.com>
2020-03-03 03:51:41 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
+  sum
 end
 
 def subtraction(num1, num2)
-
+  diff = num1 - num2
+  puts diff
+  diff
 end
 
 def division(num1, num2)
-
+  quo = num1 / num2
+  puts quo
+  quo
 end
 
 def multiplication(num1, num2)
-
+  prod = num1 * num2
+  puts prod
+  prod
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  puts mod
+  mod
 end
 
 def square_root(num)
-
+  sqr = Math.sqrt(num)
+  puts sqr
+  sqr
 end
