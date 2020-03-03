1482c0648b031f166f47e19e9dc3400f0168a55b
munificent-cast-9719 <agomezdemen15@gmail.com>
2020-03-03 03:46:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,31 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  diff = num1 - num2
+  puts diff
+  diff
 end
 
 def division(num1, num2)
-
+  quo = num1 / num2
+  puts quo
+  quo
 end
 
 def multiplication(num1, num2)
-
+  prod = num1 * num2
+  puts prod
+  prod
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  puts mod
+  mod
 end
 
 def square_root(num)
-
+  sqr = num**
+  puts sqr
+  sqr
 end
